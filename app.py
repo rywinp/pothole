f04479e9567c3d9da5d0a6f38a4a95cd860236e9
rywinp <rywinp@umich.edu>
2024-10-12 17:00:42 -0400

    Created templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from api import api
 
 app = Flask(__name__)
@@ -7,4 +7,4 @@ app.register_blueprint(api)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template("main.html")
